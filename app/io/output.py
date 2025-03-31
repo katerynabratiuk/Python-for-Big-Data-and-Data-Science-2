811fe1fe44278f1eaa43c2d0c7b82bdf99962b24
Kateryna Bratiuk <kate.bratiuk@gmail.com>
2025-03-31 21:18:21 +0300

    Add docstring for print_to_console function

--- app/io/output.py
+++ app/io/output.py
@@ -1,4 +1,13 @@
 def print_to_console(s):
+    """
+    Prints argument string to the console.
+
+    Args:
+        s (str): String to be printed.
+
+    Returns:
+        None
+    """
     pass
 
 
