f6a53e1b6dd49947f721f843a504bdf8614d4168
Kateryna Bratiuk <kate.bratiuk@gmail.com>
2025-03-31 21:07:56 +0300

    Add docstring for read_file function

--- app/io/input.py
+++ app/io/input.py
@@ -9,6 +9,18 @@ def read_input():
 
 
 def read_file(file_path):
+    """
+    Reads file's content and returns it as string.
+
+    Args:
+        file_path (str): The path to the file that needs to be read.
+
+    Returns:
+        str: Content of the file. Empty string if file is empty.
+
+    Raises:
+        FileNotFound: File or directory does not exist.
+    """
     pass
 
 
