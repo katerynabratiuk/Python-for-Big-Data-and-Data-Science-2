4fd30a79a6cf9d561a6ef153076b715b1d2cf044
Kateryna Bratiuk <kate.bratiuk@gmail.com>
2025-03-31 21:20:47 +0300

    Add docstring for write_to_file function

--- app/io/output.py
+++ app/io/output.py
@@ -12,4 +12,17 @@ def print_to_console(s):
 
 
 def write_to_file(s, file_path):
+    """
+    Reads file's content and returns it as string.
+
+    Args:
+        s (str): String that is going to be written to the file.
+        file_path (str): The path to the file that needs to be read.
+
+    Returns:
+        None
+
+    Raises:
+        FileNotFound: File or directory does not exist.
+    """
     pass
