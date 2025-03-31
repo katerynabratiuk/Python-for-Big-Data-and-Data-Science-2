b4b53467332e791af9c75d3e66b0be1abfb6f1d3
Kateryna Bratiuk <kate.bratiuk@gmail.com>
2025-03-31 22:03:23 +0300

    Add calls of the functions from the input module

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
+import app.io.input as ipt
+
+
 def main():
-    pass
+
+    # read_input
+    res = ipt.read_input()
+    print(res)
+    with open("data/res.txt", "a") as file:
+        file.write(res)
+
+    # read_file
+    content = ipt.read_file("data/random_text.txt")
+    print(content)
+    with open("data/res.txt", "a") as file:
+        file.write(content)
+
+    # read_file_pd
+    content_pd = ipt.read_file_pd("data/random_table.txt")
+    print(content_pd)
+    with open("data/res.txt", "a") as file:
+        file.write(content_pd)
 
 
 if __name__ == "__main__":
